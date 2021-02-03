603df77e9d029042681069749f52bc85b4f32c0b
oneNutW0nder <scb5436@rit.edu>
2021-02-03 10:21:48 -0500

    argument parsing completed

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,25 +1,33 @@
 #include <iostream>
 #include "simpleHttp.hpp"
+#include "util.hpp"
+#include "argparse.hpp"
 
-int main(int argc, char *argv[]) {
+int main(int argc, const char *argv[]) {
 
-    // parse command line args and validate
+    ArgumentParser parser;
+    parser.addFinalArgument("url");
+
+    // Usage will be printed automatically if invalid format
+    parser.parse(argc, argv);
+    auto url = parser.retrieve<std::string>("url");
+
+
+    parseUrl("https://www.rit.edu/");
 
     // parse URL entered in argv
-    std::cout << "Hello!" << std::endl;
-
-    // create Request object based on parsed URL
-    auto myReq = http::Request("www.rit.edu", "http");
-    myReq.SetMTls(false);
-    myReq.render();
-
-    std::cout << "Sending reqest?" << std::endl;
-
-    auto resp = myReq.sendRequest();
-
-    for (auto i: resp) {
-        std::cout << i;
-    }
+//    std::cout << "Hello!" << std::endl;
+//
+//    // create Request object based on parsed URL
+//    auto myReq = http::Request("www.rit.edu", "http");
+//    myReq.SetMTls(false);
+//    myReq.render();
+//
+//    auto resp = myReq.sendRequest();
+//
+//    for (auto i: resp) {
+//        std::cout << i;
+//    }
 
     // parse html response
 
