7b84354b05bc89241f73205dbb0abe769dfd7940
oneNutW0nder <scb5436@rit.edu>
2021-02-03 13:43:38 -0500

    Handles parsing input, sending reqests
    All that is left is handling redirects and parsing the response

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -2,6 +2,7 @@
 #include "simpleHttp.hpp"
 #include "util.hpp"
 #include "argparse.hpp"
+#include <map>
 
 int main(int argc, const char *argv[]) {
 
@@ -13,18 +14,25 @@ int main(int argc, const char *argv[]) {
     auto url = parser.retrieve<std::string>("url");
 
 
-    parseUrl("https://www.rit.edu/");
+    std::map<std::string, std::string> urlInfo = parseUrl(url);
+    for( auto i : urlInfo){
+        std::cout << i.first << " :: " << i.second << std::endl;
+    }
 
-    // parse URL entered in argv
-//    std::cout << "Hello!" << std::endl;
-//
-//    // create Request object based on parsed URL
-//    auto myReq = http::Request("www.rit.edu", "http");
-//    myReq.SetMTls(false);
-//    myReq.render();
-//
-//    auto resp = myReq.sendRequest();
-//
+    auto myReq = http::Request(urlInfo["host"], urlInfo["port"]);
+
+    if(urlInfo["scheme"] == "https")
+        myReq.SetMTls(true);
+    else
+        myReq.SetMTls(false);
+
+    myReq.SetMPath(urlInfo["path"]);
+    myReq.render();
+
+    std::cout << myReq.GetMReq() << std::endl;
+
+    auto resp = myReq.sendRequest();
+
 //    for (auto i: resp) {
 //        std::cout << i;
 //    }
