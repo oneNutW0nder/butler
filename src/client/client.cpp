177aed3911f813a58f4485b79dd0e7e6e3eb4469
oneNutW0nder <scb5436@rit.edu>
2021-02-03 21:51:56 -0500

    all working except parsing out the html body

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "simpleHttp.hpp"
-#include "util.hpp"
 #include "argparse.hpp"
 #include <map>
 
@@ -8,36 +7,22 @@ int main(int argc, const char *argv[]) {
 
     ArgumentParser parser;
     parser.addFinalArgument("url");
-
-    // Usage will be printed automatically if invalid format
     parser.parse(argc, argv);
+
     auto url = parser.retrieve<std::string>("url");
+    auto myReq = http::Request();
 
-
-    std::map<std::string, std::string> urlInfo = parseUrl(url);
-    for( auto i : urlInfo){
-        std::cout << i.first << " :: " << i.second << std::endl;
-    }
-
-    auto myReq = http::Request(urlInfo["host"], urlInfo["port"]);
-
-    if(urlInfo["scheme"] == "https")
-        myReq.SetMTls(true);
-    else
-        myReq.SetMTls(false);
-
-    myReq.SetMPath(urlInfo["path"]);
+    myReq.parseUrl(url);
     myReq.render();
 
-    std::cout << myReq.GetMReq() << std::endl;
-
     auto resp = myReq.sendRequest();
-
-//    for (auto i: resp) {
-//        std::cout << i;
-//    }
+    std::cout << myReq.GetMReq() << std::endl;
+    std::string stringResp(resp.begin(), resp.end());
+    std::cout << stringResp << std::endl;
 
     // parse html response
 
+
     return 0;
 }
+
