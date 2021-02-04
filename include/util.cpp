d0e79c6f5493df134d70ca3de4fc94a74b8a2739
oneNutW0nder <scb5436@rit.edu>
2021-02-03 21:51:41 -0500

    reorg some functions

--- include/util.cpp
+++ include/util.cpp
@@ -2,105 +2,16 @@
 #include <regex>
 
 // trim from end of string (right)
-inline std::string& rtrim(std::string& s, const std::string& t){
-    int loc = s.rfind(t);
-    s.erase(loc, t.length());
+std::string& rtrim(std::string& s, const std::string& t){
+    s.erase(s.rfind(t), t.length());
     return s;
 }
 
 // trim from beginning of string (left)
-inline std::string& ltrim(std::string& s, const std::string& t){
-    int loc = s.find(t);
-    s.erase(loc, t.length());
+std::string& ltrim(std::string& s, const std::string& t){
+    s.erase(s.find(t), t.length());
     return s;
 }
-
-
-std::map<std::string, std::string> parseUrl(std::string &url){
-
-    std::string scheme;
-    std::string host;
-    std::string port;
-    std::string path;
-    std::string other;
-
-    int loc;
-
-    if ((loc = url.find("https://")) != std::string::npos) {
-        scheme = "https";
-        port = "443";
-        url = ltrim(url, "https://");
-    }
-    else if ((loc = url.find("http://")) != std::string::npos){
-        scheme = "http";
-        port = "80";
-        url = ltrim(url, "http://");
-    }
-    else{
-        // This block handles receiving a path such as /absolute/path or "relative/path"
-        // TODO:
-    }
-
-    // No path remaining in URL so we can setup defaults
-    // Ex: url = www.rit.edu
-    if ((loc = url.find("/")) == std::string::npos){
-        host = url;
-        path = "/";
-        url = ltrim(url, host);
-    }else{
-        host = url.substr(0, loc);
-        path = url.substr(loc, url.length());
-        url = ltrim(url, host);
-        url = ltrim(url, path);
-    }
-
-    // Check for nonstandard port
-    if ((loc = host.find(":")) != std::string::npos){
-        port = host.substr(loc+1, host.length());
-        url = rtrim(url, port);
-    }
-
-    // Return anything that wasn't parsed for the user to handle
-    if(!url.empty()){
-        other = url;
-    }
-
-    return {{"scheme", scheme},
-            {"host", host},
-            {"port", port},
-            {"path", path},
-            {"other", other}};
-}
-
-std::map<std::string, std::string> parseHeaders(std::string& req){
-
-    int loc_x;
-    int loc_y;
-
-    // Return an empty map if we don't find the end of the headers
-    if((loc_x = req.find("\r\n\r\n")) == std::string::npos){
-        return {};
-    }
-
-    std::map<std::string, std::string> headers;
-    std::string tmp;
-
-    while((loc_x = req.find("\r\n")) != std::string::npos){
-        if (loc_x == 0){
-            break;
-        }
-        tmp = req.substr(0, loc_x);
-//        tmp = ltrim(tmp, ":");
-        // If we are somehow not in a valid header this will fail
-        loc_y = tmp.find(" ");
-        headers[tmp.substr(0, loc_y)] = tmp.substr(loc_y + 1, tmp.length()-2);
-
-        req = ltrim(req, (tmp + "\r\n"));
-    }
-
-    return headers;
-}
-
 
 
 //}
--- include/util.hpp
+++ include/util.hpp
@@ -5,24 +5,7 @@
 #include <string>
 #include <map>
 
-inline std::string& rtrim(std::string& s, const std::string& t);
-inline std::string& ltrim(std::string& s, const std::string& t);
-
-/**
- * Take in a URL (Ex. https://www.rit.edu)
- * Parse and split into:
- *      Scheme: https
- *      Host: www.rit.edu
- *      Path: Default = /
- *
- * @param url
- */
-std::map<std::string, std::string> parseUrl(std::string &url);
-
-/**
- * Take in the entire request and return the headers with associated values
- * @param req
- */
-std::map<std::string, std::string> parseHeaders(std::string& req);
+std::string& rtrim(std::string& s, const std::string& t);
+std::string& ltrim(std::string& s, const std::string& t);
 
 #endif
