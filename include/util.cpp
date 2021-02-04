22e1c1f967645a9c4c586601099b4b53844b637c
oneNutW0nder <scb5436@rit.edu>
2021-02-04 10:31:05 -0500

    remove uneeded code

--- include/util.cpp
+++ include/util.cpp
@@ -13,18 +13,3 @@ std::string &ltrim(std::string &s, const std::string &t) {
 }
 
 
-//}
-//
-//void parseHtml(){
-//    // Regex from RFC 3986 (https://tools.ietf.org/html/rfc3986#appendix-B)
-////    std::regex rgx("^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\\?([^#]*))?(#(.*))?" );
-//    std::regex rgx("^((\\w+):)?(\\/\\/((\\w+)?(:(\\w+))?@)?([^\\/\?:]+)(:(\\d+))?)?(\\/?([^\\/\\?#][^\\?#]*)?)?(\\?([^#]+))?(#(\\w*))?");
-//    std::smatch matches;
-//    if (std::regex_search(url, matches, rgx)) {
-//        std::cout << "Text contains the phrase 'regular expressions'\n";
-//        for (size_t i = 0; i < matches.size(); ++i) {
-//            if(matches[i].str().length() < 2) {
-//                std::cout << i << ": '" << matches[i].str() << "'\n";
-//            }
-//        }
-//    }
