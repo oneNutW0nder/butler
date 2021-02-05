f5323111e1e04e5f4bf6d513583c5ba1020f07ac
Simon B <scb5436@rit.edu>
2021-02-05 15:02:25 -0500

    setup script for ubuntu 18

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#Update
+sudo apt update && sudo apt upgrade -y
+
+# Install cmake from snap
+sudo snap install cmake
+
+# Install libs
+sudo apt install -y make libssl-dev libfmt-dev gcc build-essential
