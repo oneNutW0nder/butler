105e07f80f59778a629246aa5ef9e445d7518633
Simon B <scb5436@rit.edu>
2021-02-09 15:01:43 -0500

    create split function

--- include/util.cpp
+++ include/util.cpp
@@ -1,5 +1,10 @@
 #include "util.hpp"
 
+void gotError(const std::string &msg, const int &err){
+    std::cerr << msg << std::endl;
+    exit(err);
+}
+
 // trim from end of string (right)
 std::string &rtrim(std::string &s, const std::string &t) {
     s.erase(s.rfind(t), t.length());
@@ -12,4 +17,36 @@ std::string &ltrim(std::string &s, const std::string &t) {
     return s;
 }
 
+// pass string and get vector back of elements split by delim
+std::vector<std::string> split(std::string &s, std::string delim){
+    int loc;
+    std::vector<std::string> tokens;
+    while(!s.empty()){
+        if((loc = s.find(delim)) == std::string::npos){
+            // No more delims, push_back the remaining string and break
+            tokens.push_back(s);
+            break;
+        }
+        tokens.push_back(s.substr(0, loc));
+        s.erase(0, loc+1);
+    }
+
+    return tokens;
+}
+
+bool validateRequestLine(std::string reqLine) {
+    // From RFC 7230
+    // request-line   = method SP request-target SP HTTP-version CRLF
+    //                    *                                       *
+    // A "*" means the field above has been validated so far during execution
+    // TODO: Might need to validate the \r\n as well
+
+    std::cout << reqLine << std::endl;
+    auto tokens = split(reqLine, "\r\n");
+
+    for (auto i : tokens){
+        std::cout << i << " : " << i.length()<< std::endl;
+    }
+    return true;
+}
 
--- include/util.hpp
+++ include/util.hpp
@@ -4,9 +4,16 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <vector>
+
+void gotError(const std::string &msg, const int &err);
 
 std::string &rtrim(std::string &s, const std::string &t);
 
 std::string &ltrim(std::string &s, const std::string &t);
+
+std::vector<std::string> split(std::string &s, std::string &delim);
+
+bool validateRequestLine(std::string reqLine);
 
 #endif
