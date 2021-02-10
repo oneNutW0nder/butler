78165ce04185a6d310d535ce51477fb502b30eac
Simon B <scb5436@rit.edu>
2021-02-09 22:03:45 -0500

    reorg

--- include/util.cpp
+++ include/util.cpp
@@ -17,36 +17,5 @@ std::string &ltrim(std::string &s, const std::string &t) {
     return s;
 }
 
-// pass string and get vector back of elements split by delim
-std::vector<std::string> split(std::string &s, std::string delim){
-    int loc;
-    std::vector<std::string> tokens;
-    while(!s.empty()){
-        if((loc = s.find(delim)) == std::string::npos){
-            // No more delims, push_back the remaining string and break
-            tokens.push_back(s);
-            break;
-        }
-        tokens.push_back(s.substr(0, loc));
-        s.erase(0, loc+1);
-    }
 
-    return tokens;
-}
-
-bool validateRequestLine(std::string reqLine) {
-    // From RFC 7230
-    // request-line   = method SP request-target SP HTTP-version CRLF
-    //                    *                                       *
-    // A "*" means the field above has been validated so far during execution
-    // TODO: Might need to validate the \r\n as well
-
-    std::cout << reqLine << std::endl;
-    auto tokens = split(reqLine, "\r\n");
-
-    for (auto i : tokens){
-        std::cout << i << " : " << i.length()<< std::endl;
-    }
-    return true;
-}
 
--- include/util.hpp
+++ include/util.hpp
@@ -3,8 +3,6 @@
 
 #include <iostream>
 #include <string>
-#include <map>
-#include <vector>
 
 void gotError(const std::string &msg, const int &err);
 
@@ -12,8 +10,6 @@ std::string &rtrim(std::string &s, const std::string &t);
 
 std::string &ltrim(std::string &s, const std::string &t);
 
-std::vector<std::string> split(std::string &s, std::string &delim);
 
-bool validateRequestLine(std::string reqLine);
 
 #endif
