b79457b4c5e2fc298b1be8653d4cf99aa0f0c07f
Simon B <scb5436@rit.edu>
2021-02-10 13:29:51 -0500

    reorg

--- include/util.cpp
+++ include/util.cpp
@@ -17,5 +17,22 @@ std::string &ltrim(std::string &s, const std::string &t) {
     return s;
 }
 
+// pass string and get vector back of elements split by delim
+std::vector<std::string> split(std::string &s, std::string delim){
+    int loc;
+    std::vector<std::string> tokens;
+    while(!s.empty()){
+        if((loc = s.find(delim)) == std::string::npos){
+            // No more delims, push_back the remaining string and break
+            tokens.push_back(s);
+            break;
+        }
+        tokens.push_back(s.substr(0, loc));
+        s.erase(0, loc+1);
+    }
+
+    return tokens;
+}
+
 
 
--- include/util.hpp
+++ include/util.hpp
@@ -3,8 +3,11 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 void gotError(const std::string &msg, const int &err);
+
+std::vector<std::string> split(std::string &s, std::string delim);
 
 std::string &rtrim(std::string &s, const std::string &t);
 
