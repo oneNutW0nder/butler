a07b12270945a0666c1121e049e6571b289d565c
Simon B <scb5436@rit.edu>
2021-02-09 22:03:03 -0500

    updates

---
+++ include/httpParser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by simon on 2/9/2021.
+//
+
+#include "httpParser.h"
---
+++ include/httpParser.hpp
@@ -0,0 +1,14 @@
+//
+// Created by simon on 2/9/2021.
+//
+
+#ifndef BUTLER_CLIENT_HTTPPARSER_H
+#define BUTLER_CLIENT_HTTPPARSER_H
+
+
+class httpParser {
+
+};
+
+
+#endif //BUTLER_CLIENT_HTTPPARSER_H
