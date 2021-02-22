1ea7ceb6fe46b545aa7768a282ffc33b138e237e
oneNutW0nder <scb5436@rit.edu>
2021-02-22 16:47:04 -0500

    threading responses

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <csignal>
 #include <string>
+#include <thread>
 
 #include <unistd.h>
 #include <fmt/format.h>
 
 #include "argparse.hpp"
 #include "simpleServer.hpp"
-#include "httpParser.hpp"
 
 /**
  * This program is an HTTP/HTTPS server. Adapted from Arthur O'Dwyer's
@@ -72,44 +72,16 @@ int main(const int argc, const char *argv[]){
     std::cout << "Press Ctrl + C to shutdown" << std::endl;
 
     // Start the server loop!
-    while (auto bio = server::new_connection(listenBio.get())){
+    while (auto bio = server::new_connection(listenBio.get())) {
         // If HTTPS push an SLL BIO into the chain
         if (https) {
             bio = std::move(bio) | server::UniquePtr<BIO>(BIO_new_ssl(ctx.get(), 0));
         }
-        try {
-            // Read request and validate
-            std::string req = server::receive_http_message(bio.get(), https);
-            std::cout << req << std::endl << std::endl;
 
-
-            // Validate and get resource/params
-            auto valid = httpParser::Validator(req);
-            // TODO: Log this information to a file
-            std::cout << fmt::format("[+] Received request: {} {}", valid.GetMMethod(), valid.GetMReqTarget()) << std::endl;
-            auto resources = server::parseResource(valid.GetMReqTarget(), valid.GetMAbsoluteUri());
-
-            // Create a requestInfo struct to pass around
-            server::requestInfo reqInfo;
-            reqInfo.method = valid.GetMMethod();
-            reqInfo.resource = resources.first;
-            reqInfo.params = {};   // TODO: Will be dependent on parsing params from request-target URIs
-            reqInfo.body = valid.GetMBody();
-            reqInfo.serverRoot = serverRoot;
-
-            auto resp = server::serveRequest(&reqInfo);
-            server::sendTo(bio.get(), resp);
-        }
-        // Catch custom server exceptions that contain info about error type and message
-        catch (server::httpException& e) {
-            auto resp = server::makeResponse(std::to_string(e.GetMStatusCode()), e.GetMCodeMsg(), e.GetMErrMsg(), {});
-            server::sendTo(bio.get(), resp);
-        }
-        // Catch all other exceptions and respond with 500 code
-        catch (...) {
-            auto resp = server::makeResponse("500", "Internal Server Error", "General Error", {});
-            server::sendTo(bio.get(), resp);
-        }
+        // Thread the responding of requests
+        // Openssl BIOs support multiple connections with this method
+        std::thread i(server::requestHandler, std::move(bio), serverRoot, std::ref(https));
+        i.join();
 
     }
 
